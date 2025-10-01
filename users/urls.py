7d27f39064de76bb1ab00ca0f8883b0c8a001a41
ngabroger <arbination13@gmail.com>
2025-10-01 09:10:47 +0700

    feat: add dashboard view to user URLs and import render in views

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import FakerUserView
-
+from .views import FakerUserView, dashboard_view
 urlpatterns = [
     path('faker-users/', FakerUserView.as_view(), name='faker-users'),
+    path('dashboard/', dashboard_view, name='dashboard'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from faker import Faker
+from django.shortcuts import render
 import random
 import math
 
